22a890c362081a5c3a2166da04b30833eacece7c
Harsh Suthar <byharshsuthar@gmail.com>
2026-05-07 18:36:34 +0530

    create the initial version of layout screen for ui

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+import "./index.css";
+import App from "./App.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(<App />);
