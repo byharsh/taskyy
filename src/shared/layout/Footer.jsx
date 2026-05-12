c65afb909825449e20d3ca4311a7da225fc1f0a5
Harsh Suthar <byharshsuthar@gmail.com>
2026-05-12 11:07:55 +0530

    Footer created and added in the main screen

--- src/shared/layout/Footer.jsx
+++ src/shared/layout/Footer.jsx
@@ -1,5 +1,15 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="h-8 w-full bg-yellow-100 opacity-70  flex justify-between items-center px-6 text-gray-400">
+      <div className="flex gap-4 text-xs tracking-wider ">
+        <span>Roadmap</span>
+        <span>How-to</span>
+      </div>
+      <span className="text-xs tracking-wider">
+        © 2026 Taskyy. Made with ❤️ and ⚡
+      </span>
+    </div>
+  );
 };
 
 export default Footer;
